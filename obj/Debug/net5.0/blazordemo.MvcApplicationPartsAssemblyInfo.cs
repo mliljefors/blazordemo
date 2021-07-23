26b1c1b428f5cf85a47438c6ba27740997f700e0
Mark Liljefors <mliljefors@utilismartcorp.com>
2021-07-23 09:50:04 -0700

    added BlazorEngine library

--- obj/Debug/net5.0/blazordemo.MvcApplicationPartsAssemblyInfo.cs
+++ obj/Debug/net5.0/blazordemo.MvcApplicationPartsAssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("BlazorEngine")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI.Views.V4")]
 
